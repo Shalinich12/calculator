478f0fe5f9616786ad54491b8a5f3be984213b6f
Shalinich12 <shalinichellingi9@gmail.com>
2025-04-09 14:22:37 +0530

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero/Hero'
+import "./App.css"
+import Testmonials from './Testimonials/Testimonials'
+
+export default function App() {
+  return (
+    <div className='mainCon'>
+        <Hero />
+        <Testmonials />        
+    </div>
+  )
+}
+
+
